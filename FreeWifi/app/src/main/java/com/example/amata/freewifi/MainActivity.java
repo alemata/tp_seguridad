7bf000d7f2d10ac9baa4eb79954956daee9e3791
Alejandro Mataloni <amataloni@gmail.com>
2016-05-13 00:50:25 -0300

    removiendo variable no usada

--- FreeWifi/app/src/main/java/com/example/amata/freewifi/MainActivity.java
+++ FreeWifi/app/src/main/java/com/example/amata/freewifi/MainActivity.java
@@ -7,14 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-
-    private List<File> photos = new ArrayList<File>();
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
